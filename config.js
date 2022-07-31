0307d5f5e69247fb92dd80aafa59f4117624451f
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-07-31 09:41:38 +0700

    Update config.js
    Signed-off-by: Kian <108917081+kontobbbsbaan@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-global.owner = ["123@s.whatsapp.net"]
-global.multiPrefix = false
+global.owner = ["6283833694338@s.whatsapp.net"]
+global.multiPrefix = true
 global.autoRead = true
 global.messConf = {
   prefix: "!",
-  maxFileSize: 30.0
+  maxFileSize: 99999.0
 }
 global.language = 'ind'
 global.allow = {
   nsfw: true
 }
 global.stickerInfo = {
-  author: "© Bot",
-  pack: ""
+  author: "© Ɏåkµzå M∂",
+  pack: "Ɏåkµzå M∂"
 }
-global.gameTime = 60
+global.gameTime = 75
 // Welcome option:
 // @user = tag user
 global.initialWelcome = `Welcome @user >///<`
